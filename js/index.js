4e424e5deb379a679b47ee220b03c40e95df0c4d
Irina Brennen <irinabrennen@twc.com>
2018-05-23 23:34:21 -0500

    added data-fips and data-educatin propertl to each path

--- js/index.js
+++ js/index.js
@@ -15,4 +15,14 @@ const svg = d3.select("body")
            .data(geojson.features)
            .enter().append("path")
              .attr("d",geoPath)
-             .attr("class", "county");
+             .attr("class", "county")
+             .attr("data-fips", d => d.id)
+             .attr("data-education", 
+                    d => {
+                      var el =  educationData.find(function(element) {
+                        return (element.fips == d.id) ;                  
+                    });
+
+                    return el.bachelorsOrHigher;
+
+                  });
