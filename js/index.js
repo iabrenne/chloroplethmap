bd50a4efd6a161fb6e9781b0ce8b8fad06853681
Irina Brennen <irinabrennen@twc.com>
2018-05-25 19:57:24 -0500

    passing 2nd tooltip test now :)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 const w = 1500;
 const h = 750;
 const geoPath = d3.geoPath();
-var geojson = topojson.feature(geoTopology, geoTopology.objects.counties);
+const geojson = topojson.feature(geoTopology, geoTopology.objects.counties);
+
+
+const returnEductionData = d => {
+  let el =  educationData.find(function(element) {
+    return (element.fips == d.id) ;                  
+  });
+
+return el.bachelorsOrHigher;
+};
+
 
 //Create SVG element
 const svg = d3.select("body")
@@ -17,25 +27,16 @@ const svg = d3.select("body")
            .attr("d",geoPath)
            .attr("class", "county")
            .attr("data-fips", d => d.id)
-           .attr("data-education", 
-              d => {
-                var el =  educationData.find(function(element) {
-                  return (element.fips == d.id) ;                  
-              });
+           .attr("data-education", d => returnEductionData(d))
+           .on("mouseover",(d)=>{ 
 
-              return el.bachelorsOrHigher;
-            })
-          .on("mouseover",(d)=>{ 
-
-              let tooltipElem = document.getElementById("tooltip");
-          
+              let tooltipElem = document.getElementById("tooltip");          
               tooltipElem.style.display = "block";
               
   //            tooltipElem.innerText = `${getMonth(d.month)} ${d.year}\n` ;
 
               tooltipElem.innerText = `${d.id}\n` ;
-          
-//              tooltipElem.setAttribute("data-year", d.year);
+              tooltipElem.setAttribute("data-education", returnEductionData(d));
           
               // tooltipElem.style.left= (xScale(d.year) + tooltipOffsetHorizontal) + "px";
               // tooltipElem.style.top = (yScale(d.month) + tooltipOffsetVertical) + "px";
